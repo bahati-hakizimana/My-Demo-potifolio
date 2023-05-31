2942aeb1aeb61cbdc10f24cb6c4bd6071b884192
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2023-05-31 12:41:05 +0200

    third commit

---
+++ src/sections/services/data.js
@@ -0,0 +1,14 @@
+import{SiAdobexd} from 'react-icons/si'
+import{RiReactjsLine} from 'react-icons/ri'
+import{FaServer} from 'react-icons/fa'
+import{AiFillAppstore} from 'react-icons/ai'
+
+
+const data = [
+    {id: 1, icon:<SiAdobexd/>, title: 'UI/UX', desc: 'My design are mordern and in tuitive' } , 
+    {id: 2, icon:<RiReactjsLine/>, title:  'Frontend Development', desc: 'Your Product will look goodand will be accessible on all devices including Phones, Tablets, and desktop.'} , 
+    {id: 3, icon:<FaServer/>, title: 'Backend Development', desc:'The security of your busness/product taken seriuosly right from the start of the project.I will make sure that your website is secure from attacks.' },
+    {id: 3, icon:<AiFillAppstore/>, title: 'App Development', desc:'Unlike other Developers in the market. I will build an app that runs on both IOS and Android devices without any an extra cost to you' } 
+    
+]
+export default data
